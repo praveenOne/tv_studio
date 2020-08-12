f8ceb4701b4cca18480127cde45e08401377cfe6
Praveen Liyanaarachchi <hello@praveen.one>
2020-08-12 11:34:52 +0900

    merge conflicts resolved

--- scenes/forest.h
+++ scenes/forest.h
@@ -1,89 +1,63 @@
 #include <functional>
 #include <iostream>
 
-#include "../sdladapters.h"
-#include "../scene.h"
-#include "../camera.h"
-#include "../sprite.h"
-#include "../move.h"
-#include "../units.h"
+#include "../src/sdladapters.h"
+#include "../src/scene.h"
+#include "../src/camera.h"
+#include "../src/sprite.h"
+#include "../src/move.h"
+#include "../src/units.h"
+#include "../src/character.h"
+
+typedef Character<sdl::Renderer, sdl::EventPump::EventType> CharacterType;
+
+sdl::UsesSDL usesSdl;
 
 class ForestScene {
 public:
     static void run() {
-        sdl::Window win("Runner?", 700, 272*2);
-        sdl::EventPump pump; // event system sdl 
-        auto scene1 = std::make_shared<Scene>(960, 272*2, 5);
-        scene1->at(4)->background("rsrc/sprites/backgrounds/bg.png");
-        scene1->at(3)->background("rsrc/sprites/backgrounds/far_trees.png");
-        scene1->at(2)->background("rsrc/sprites/backgrounds/mid_trees.png");
-        scene1->at(0)->background("rsrc/sprites/backgrounds/close_trees.png");
-        auto renderer{ win.renderer() };
-        Camera cam1(renderer);
-        cam1.scene(scene1.get());
+   
+    try
+    {
+        sdl::EventPump pump;
+        sdl::Window win("Runner?", 700, 272*2, pump);
+        CharacterType::SceneType scene1(960, 272*2, 5);
+        scene1.at(4)->background(std::string("rsrc/sprites/backgrounds/bg.png"));
+        scene1.at(3)->background(std::string("rsrc/sprites/backgrounds/far_trees.png"));
+        scene1.at(2)->background(std::string("rsrc/sprites/backgrounds/mid_trees.png"));
+        scene1.at(0)->background(std::string("rsrc/sprites/backgrounds/close_trees.png"));
+        auto renderer{win.renderer()}; // uniform initialization systax
+        Camera<CharacterType::RendererType, CharacterType::SceneType> cam1(renderer);
+        cam1.scene(&scene1);
 
-        //close trees
-        //ice
-        //cat
-        //mid trees
-        //far trees
-        //background
-
-        //scene1->at(5)->add(std::move(drawBackgroundScene(renderer)));
-        //drawBackgroundScene();
-
-        // auto close_trees = std::make_unique<Character>();
-        // close_trees->_position = {0, 0, 960, 272*2};
-        // Sprite sprite_close_trees(*renderer, "rsrc/sprites/backgrounds/close_trees.png");
-        // sprite_close_trees.setupCharacter(*close_trees);
-        // scene1->at(1)->add(std::move(close_trees));
-
-        // auto far_trees = std::make_unique<Character>();
-        // far_trees->_position = {0, 0, 960, 272*2};
-        // Sprite sprite_far_trees(*renderer, "rsrc/sprites/backgrounds/far_trees.png");
-        // sprite_far_trees.setupCharacter(*far_trees);
-        // scene1->at(1)->add(std::move(far_trees));
-
-
-
-
-
-        // ice cube
         int x = 1000;
-        Character *pIce = nullptr;
-        Sprite sprite_ice(*renderer, "rsrc/ice-block.png"); // sprite is the skin of the character
-        {
-            auto ice = std::make_unique<Character>();
-            pIce = &(*ice);
-            ice->_position ={ x, 300, 80, 80 };
-            sprite_ice.setupCharacter(*ice);
-            // save the ice character pointer before giving it away
-            scene1->at(1)->add(std::move(ice)); // plane 0 is the one is closest
-        }
+        auto ice = std::make_shared<CharacterType>();
+        auto p = ice.get();
+        p->_position = {x, 300, 80, 80};
+        Sprite sprite_ice(*renderer, "rsrc/ice-block.png");
+        sprite_ice.setupCharacter(*p);
+        scene1.at(1)->push_back(ice);
 
-        // cat
-        auto cat = std::make_unique<Character>();
-        cat->_position ={ 0, 300, 69, 60 };
+        auto cat = std::make_shared<CharacterType>();
+        cat->_position = {0, 300, 69, 60};
         Sprite sprite_cat(*renderer, "rsrc/cat-tile.png", 10, 8, 46, 53, 95);
         sprite_cat.setupCharacter(*cat);
-        HMove(pIce->_position, 0, units::Speed::MetresPerSecond(9.0), *cat);
-        scene1->at(2)->add(std::move(cat));
+        HMove(p->_position, 0, units::Speed::MetresPerSecond(9.0), *cat);
+        scene1.at(2)->push_back(cat);
 
-        // auto mid_trees = std::make_unique<Character>();
-        // mid_trees->_position = {0, 0, 960, 272*2};
-        // Sprite sprite_mid_trees(*renderer, "rsrc/sprites/backgrounds/mid_trees.png");
-        // sprite_mid_trees.setupCharacter(*mid_trees);
-        // scene1->at(3)->add(std::move(mid_trees));
-
-        // run the game!
         pump.run(
             [&]() {
                 cam1.update();
-                scene1->update();
+                scene1.update();
                 cam1.render();
             },
             16,
-                std::bind(&Scene::handle_event, scene1, std::placeholders::_1));
+            std::bind(&CharacterType::SceneType::handle_event, &scene1, std::placeholders::_1));
+    }
+    catch (sdl::Error &err)
+    {
+        std::cerr << err.what() << std::endl;
+    }
 
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,7 @@
-#include <functional>
-#include <iostream>
-#include "sdladapters.h"
-#include "scene.h"
-#include "camera.h"
-#include "sprite.h"
-#include "move.h"
-#include "units.h"
-#include "character.h"
-
-typedef Character<sdl::Renderer, sdl::EventPump::EventType> CharacterType;
+#include "../scenes/forest.h"
 
 int main()
 {
-    sdl::UsesSDL usesSdl;
-    try
-    {
-        sdl::EventPump pump;
-        sdl::Window win("Runner?", 700, 400, pump);
-        CharacterType::SceneType scene1(7000, 400, 4);
-        scene1.back()->background(std::string("rsrc/IMG_6110.jpg"));
-        auto renderer{win.renderer()}; // uniform initialization systax
-        Camera<CharacterType::RendererType, CharacterType::SceneType> cam1(renderer);
-        cam1.scene(&scene1);
-
-        int x = 1000;
-        auto ice = std::make_shared<CharacterType>();
-        auto p = ice.get();
-        p->_position = {x, 300, 80, 80};
-        Sprite sprite_ice(*renderer, "rsrc/ice-block.png");
-        sprite_ice.setupCharacter(*p);
-        scene1.at(1)->push_back(ice);
-
-        auto cat = std::make_shared<CharacterType>();
-        cat->_position = {0, 300, 69, 60};
-        Sprite sprite_cat(*renderer, "rsrc/cat-tile.png", 10, 8, 46, 53, 95);
-        sprite_cat.setupCharacter(*cat);
-        HMove(p->_position, 0, units::Speed::MetresPerSecond(9.0), *cat);
-        scene1.at(2)->push_back(cat);
-
-        pump.run(
-            [&]() {
-                cam1.update();
-                scene1.update();
-                cam1.render();
-            },
-            16,
-            std::bind(&CharacterType::SceneType::handle_event, &scene1, std::placeholders::_1));
-    }
-    catch (sdl::Error &err)
-    {
-        std::cerr << err.what() << std::endl;
-    }
+    ForestScene::run();
     return 0;
 }
