e58e0b9194857979df2a393ce29c09f3874cd2f9
Ignacio Rodriguez <ignacio.rodriguez@bairesdev.com>
2020-08-14 14:01:30 +0700

    scenes under src; animation fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
-#include "../scenes/forest.h"
+#include "sdladapters.h"
+#include "scenes/forest.h"
+#include "scenes/ice.h"
 
 int main()
 {
+    sdl::UsesSDL use_sdl;
     ForestScene::run();
+    // IceScene::run();
     return 0;
 }
